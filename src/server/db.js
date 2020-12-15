9e0397108a5a2c78b0c8871a445729fff487f796
Ismac Flores <fismaelb@hotmail.com>
2020-12-14 21:05:25 -0600

    Database information updated

--- src/server/db.js
+++ src/server/db.js
@@ -1,5 +1,6 @@
 module.exports = {
 	//'url' : 'mongodb://<dbuser>:<dbpassword>@novus.modulusmongo.net:27017/<dbName>'
 	//'url': 'mongodb+srv://userimueble:user123@cluster0.npykh.mongodb.net'
-	'url': 'mongodb://localhost:27017/app'
+	//'url': 'mongodb://localhost:27017/app'
+	'url': 'mongodb+srv://user2inmueble:user123@cluster0.npykh.mongodb.net'
 }
